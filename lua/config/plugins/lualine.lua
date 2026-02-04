fa34cc2abfc71e93b08ca6113cdb78186492a4c6
Lackie <Justin.M.Lackie@Medstar.net>
2026-02-04 16:47:27 -0500

    added neo-tree

---
+++ lua/config/plugins/lualine.lua
@@ -0,0 +1,17 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  opts = {
+    options = {
+      theme = 'auto',
+      section_separators = '',
+      component_separators = '',
+    },
+    sections = {
+      lualine_a = { 'mode' },
+      lualine_b = { 'branch', 'diff' },
+      lualine_c = { 'filename' },
+      lualine_x = { 'filetype' },
+    },
+  },
+}
